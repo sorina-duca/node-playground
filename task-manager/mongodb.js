459cf8e14965dfd24b30b2066e409efd6509d623
sorina-duca <sorina.duca@gmail.com>
2021-08-30 12:02:25 +0200

    practice CRUD and install mongoose

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,5 @@
+const { MongoClient, ObjectId } = require('mongodb')
 
-const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -11,5 +11,43 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
+    db.collection('tasks').deleteMany({
+        description: 'write Layal'
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error)
+    })
+
+    // db.collection('tasks').updateMany({
+    //     // completed: false
+    // }, { $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
 
 })
+
+
+
+//     db.collection('users').updateOne({
+//         _id: ObjectId("612749282cc3efc6eba89da6")
+//     }, {
+//         // $set: {
+//         //     name: 'Sandra'
+//         // }
+//         $inc: {
+//             age: 1
+//         }
+//     }).then((result) => {
+//         console.log(result)
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+
+
